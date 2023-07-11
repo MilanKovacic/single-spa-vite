7b251fa7fce9d6f67719ad9dac571bed039b9f97
Milan Kovacic <MilanKovacic@users.noreply.github.com>
2023-07-11 07:47:38 +0200

    install vite packages, and adapt vite configuration for single-spa compatibility

--- microfrontend/vite.config.ts
+++ microfrontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    build: {
+      rollupOptions: {
+        preserveEntrySignatures: "strict",
+        input: "main.ts",
+        output: {
+          format: "esm",
+          entryFileNames: `[name].js`,
+        },
+        external: ["single-spa"]
+      },
+    },
+    plugins: [
+      react()
+    ],
+  }
 })
